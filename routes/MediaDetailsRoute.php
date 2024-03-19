1aa60f110c2f369b51e50fbf756f564a69dd4817
theBigTank <codeplay.erniie@gmail.com>
2024-03-19 17:16:42 +0100

    created function to handle incoming caption

--- routes/MediaDetailsRoute.php
+++ routes/MediaDetailsRoute.php
@@ -12,6 +12,17 @@ class MediaDetailsRoute {
     add_action('rest_api_init', array($this, 'register_get_media_details'));
     add_action('rest_api_init', array($this, 'register_bulk_generate'));
     add_action('rest_api_init', array($this, 'register_single_image_upload'));
+    add_action('rest_api_init', array($this, 'register_caption_retrieval'));
+  }
+
+  // Registers a REST route for retrieving media details.
+  public function register_caption_retrieval() {
+    // Define a new route in the WordPress REST API namespace 'altly/v1' with the endpoint '/get-media-details'.
+    register_rest_route('altly/v1', '/retrieve_caption', array(
+      'methods' => 'POST', // Specify that this endpoint responds to HTTP GET requests.
+      'callback' => array($this, 'handle_incoming_caption'), // Callback function to process the request.
+      'permission_callback' => '__return_true', // Permission callback to control access; here, it allows all requests.
+    ));
   }
 
   // Registers a REST route for retrieving media details.
@@ -311,8 +322,7 @@ class MediaDetailsRoute {
 
         // Encode the current item's image URL into the JSON body of the request.
         $body = json_encode([
-          'images' => $imageUrls,
-          'licenseKey' => $license_key
+          'images' => $imageUrls
         ]);
     
         // Perform a POST request to the external API with the prepared headers and body.
@@ -325,11 +335,15 @@ class MediaDetailsRoute {
         if (is_wp_error($api_response)) {
           // Extract the error message from the API response.
           $error_message = $api_response->get_error_message();
+
+          error_log('error_message: ' . print_r($error_message, true));
           return new \WP_REST_Response(['error' => $error_message], 500);
         }
 
         $api_status = wp_remote_retrieve_response_code($api_response);
         $api_data = json_decode(wp_remote_retrieve_body($api_response), true);
+
+        error_log('API Response: ' . print_r($api_data, true));
 
         if ($api_status == 200) {
           return new \WP_REST_Response(['message' => 'Images queued successfully'] + $api_data, 200);
@@ -341,6 +355,100 @@ class MediaDetailsRoute {
 
       }
     }
+  }
+
+  public function handle_incoming_caption($request) {
+    
+    $data = $request->get_json_params(); // get the images data
+    
+    $headers = $request->get_headers(); // get headers
+
+    $license_key = $headers['license_key'][0];
+
+    $isLicenseKeyValid = $this->checkLicenseKey($license_key);
+
+    // check if license key is valid
+    if ($isLicenseKeyValid) {
+      // if license key matches
+      // retrieve all missing alt text from the wordpress media library
+
+      // Set up query arguments to retrieve media attachments of type 'image/jpeg' and 'image/png'.
+      $args = array(
+        'post_type' => 'attachment', // Target media attachments.
+        'post_status' => 'inherit', // Include all attachments regardless of status.
+        'post_mime_type' => array('image/jpeg', 'image/png'), // Filter by JPEG and PNG image formats.
+        'posts_per_page' => -1, // Retrieve all matching media items.
+      );
+
+      $media_query = new \WP_Query($args);
+      $images_missing_alt_text_arr = array();
+
+      // Check if the query returned any posts (media attachments).
+      if ($media_query->have_posts()) {
+        // Loop through the media attachments.
+        while ($media_query->have_posts()) {
+          $media_query->the_post(); // Set up the global post data.
+          $attachment_id = get_the_ID(); // Retrieve the ID of the current attachment.
+          $alt_text = get_post_meta($attachment_id, '_wp_attachment_image_alt', true);
+          $image_url = wp_get_attachment_url($attachment_id); // Get the URL of the attachment.
+          $file_path = get_attached_file($attachment_id); // Retrieve the file path of the attachment.
+          // $attachment_metadata = wp_get_attachment_metadata($attachment_id);
+
+          // Compile the details for the current attachment.
+          $current_media_details = array(
+            'id' => $attachment_id,
+            'alt_text' => esc_attr($alt_text),
+            'url' => esc_url($image_url),
+            'file_path' => esc_url($file_path), // Note: Consider a different method for sanitizing file paths.
+            // 'metadata' => $attachment_metadata,
+          );
+
+          // Check if the alt text is missing and add to the missing alt text array.
+          if (empty($alt_text)) {
+            $images_missing_alt_text_arr[] = $current_media_details;
+          }
+        }
+        wp_reset_postdata(); // Reset the global post data to avoid conflicts with other queries.
+      } else {
+        // Return a response indicating that no media was found if the query returned no posts.
+        return rest_ensure_response(array('message' => 'No media found.'));
+      }
+      wp_reset_postdata(); // Reset the global post data again for good measure.
+
+      if (count($images_missing_alt_text_arr) > 0) {
+        // loop over the data and get matching image IDs
+        // Extract IDs from $images_missing_alt_text_arr
+        $missingAltTextAttachmentIDs = array_column($images_missing_alt_text_arr, 'id');
+
+        foreach ($data['images'] as $item) {
+          $attachment_id = $item['cms_id'];
+          if (in_array($attachment_id, $missingAltTextAttachmentIDs)) {
+              $altText = $item['alt_text'];
+              // $confidenceScore = $item['confidence'];
+
+              // update alt text for all images missing alts
+              update_post_meta($attachment_id, '_wp_attachment_image_alt', sanitize_text_field($altText));
+              // update_post_meta($attachment_id, 'confidence_score', sanitize_text_field($confidenceScore));
+          }
+        }
+
+      } else {
+        error_log('Error', true);
+      }
+
+    }
+
+    return new \WP_REST_Response('Success', 200);
+
+  }
+
+  protected function checkLicenseKey($license_key) {
+    if ($license_key === get_option('_altly_license_key')) {
+      // error_log('Valid license key: ' . print_r(get_option('_altly_license_key'), true));
+      return true;
+    }
+
+    return false;
   }
 
 }
