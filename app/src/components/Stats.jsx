1cf7cccd3ddb4dfd881a3687c7ed2940b97d22b3
Dalton McGee <dalton.mcgee@splitero.com>
2025-01-20 18:15:54 -0600

    update style on negative credits

--- app/src/components/Stats.jsx
+++ app/src/components/Stats.jsx
@@ -40,7 +40,7 @@ export default function Example({
         </dt>
         <dd
           className={`w-full flex-none text-3xl font-medium leading-10 tracking-tight ${
-            credits ? 'text-gray-900' : 'text-red-500'
+            credits < 1 ? 'text-gray-900' : 'text-red-500'
           }`}
         >
           {credits}
