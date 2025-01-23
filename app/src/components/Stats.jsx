7eba307a073e794eed195b1823b0bc48f2389b01
Dalton McGee <dalton.mcgee@splitero.com>
2025-01-22 18:30:24 -0600

    udpate "credits" to "images" on dashboard

--- app/src/components/Stats.jsx
+++ app/src/components/Stats.jsx
@@ -36,7 +36,7 @@ export default function Example({
       </div> */}
       <div className='flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8'>
         <dt className='text-sm font-medium leading-6 text-gray-500'>
-          Credits Remaining
+          Images Remaining
         </dt>
         <dd
           className={`w-full flex-none text-3xl font-medium leading-10 tracking-tight ${
